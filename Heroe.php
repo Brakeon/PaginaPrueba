ecc5389e97b3f7b823af432ec7afc4685c9fbe6a
Asier Abad <Asier.abad@gmail.com>
2018-09-24 17:03:00 +0200

    2

--- Heroe.php
+++ Heroe.php
@@ -1,35 +1,39 @@
-<?php 
+<?php
 
-Class Heroe{
-    var $nombre;
-    var $fecha;
-    var $poderes;
-    var $bio;
-    var $universo;
+class Heroe
+{
+    public $nombre;
+    public $fecha;
+    public $poderes;
+    public $bio;
+    public $universo;
 
-    function __construct($nombre, $fecha, $poderes, $bio, $universo)
-        {
-            $this->$nombre = $nombre;
-            $this->$fecha = $fecha;
-            $this->$poderes = $poderes;
-            $this->$bio = $bio;
-            $this->$universo = $universo;
-        }
+    public function __construct($nombre, $fecha, $poderes, $bio, $universo)
+    {
+        $this->nombre = $nombre;
+        $this->fecha = $fecha;
+        $this->poderes = $poderes;
+        $this->bio = $bio;
+        $this->universo = $universo;
+    }
 
-    function ComprobarUniverso(){
-        switch($universo){
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    public function ComprobarUniverso()
+    {
+        switch ($universo) {
             case 'dc':
-            break;
+                break;
 
             case 'marvel':
-            break;
+                break;
 
             default:
-            break;
+                break;
         }
     }
 
-    
 }
-
-?>
--- main.php
+++ main.php
@@ -1,4 +1,10 @@
-<?php
+<html>
+ <head>
+  <title>Prueba de PHP</title>
+ </head>
+ <body>
+
+ <?php
 include 'Heroe.php';
 
 $hulk = new Heroe("Hulk", 1962, "fuerza", "...", "marvel");
@@ -6,5 +12,10 @@ $spiderman = new Heroe("Spiderman", 1962, "sentido arácnido", "...", "marvel");
 $superman = new Heroe("Superman", 1933, "fuerza", "...", "dc");
 $batman = new Heroe("Batman", 1939, "ninguno", "...", "dc");
 
+$arrayHeroe = array($hulk, $spiderman, $superman, $batman);
 
+$arrayHeroe[sizeof($arrayHeroe) + 1] = new Heroe("Deadpool", 1991, "Curación", "...", "marvel");
 ?>
+
+</body>
+</html>
