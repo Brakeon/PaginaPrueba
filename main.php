6d2cfad2114de3468548fd0b0cf03869d8b7ca04
Asier Abad <Asier.abad@gmail.com>
2018-09-25 12:50:30 +0200

    bootstrap

--- main.php
+++ main.php
@@ -23,14 +23,16 @@ echo "<h1>hola\n</h1>";
 foreach($arrayHeroe as $heroe){
     echo "<h2>".$heroe->nombre."</h2>";
     ?>
-<div class="card" style="width: 18rem;">
-  <img class="card-img-top" src=".../100px180/" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+    <div class="card-deck">
+        <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+            <div class="card-body">
+                <h5 class="card-title">.$heroe->nombre.</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    </div>
    <?php
 }
 ?>
