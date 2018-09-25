1ba4f7a649085d7382bed7992dc181033134cf62
Asier Abad <Asier.abad@gmail.com>
2018-09-25 12:44:25 +0200

    bootstrap

--- main.php
+++ main.php
@@ -1,6 +1,8 @@
 <html>
  <head>
   <title>Prueba de PHP</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
  </head>
  <body>
 
@@ -20,6 +22,16 @@ echo "<h1>hola\n</h1>";
 
 foreach($arrayHeroe as $heroe){
     echo "<h2>".$heroe->nombre."</h2>";
+    ?>
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src=".../100px180/" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+   <?php
 }
 ?>
 
