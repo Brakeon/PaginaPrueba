16dc5446f2c022786a4ff574b46e4b8661c7b02d
Asier Abad <Asier.abad@gmail.com>
2018-09-25 12:34:44 +0200

    lista

--- main.php
+++ main.php
@@ -16,7 +16,11 @@ $arrayHeroe = array($hulk, $spiderman, $superman, $batman);
 
 $arrayHeroe[sizeof($arrayHeroe) + 1] = new Heroe("Deadpool", 1991, "Curación", "...", "marvel");
 
-echo "hola";
+echo "<h1>hola\n</h1>";
+
+foreach($arrayHeroe as $heroe){
+    echo "<h2>".$heroe->nombre."</h2>";
+}
 ?>
 
 </body>
