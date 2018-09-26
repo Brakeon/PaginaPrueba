0a9a7475958024fb784a5dc220045a6e045e1dfc
Asier Abad <Asier.abad@gmail.com>
2018-09-26 13:54:31 +0200

    sql

--- SQL.php
+++ SQL.php
@@ -6,21 +6,22 @@ class DB
 {
     public function iniciar()
     {
-        
+
     }
     public function __construct()
     {
-        $mysqli = mysqli_connect('localhost', 'asier', 'asier', 'Superheroes', 3306);
+        $mysqli = new mysqli_connect('localhost', 'asier', 'asier', 'Superheroes', 3306);
 
         if (!$mysqli) {
             echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
             echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
             echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
             exit;
+        } else {
+            return $mysqli;
         }
-        return $mysqli;
     }
-    public function Consulta(/*$mysqli*/)
+    public function Consulta($mysqli)
     {
         $resultado = $mysqli->query("SELECT * FROM Heroes");
         // $fila = $resultado->fetch_assoc();
