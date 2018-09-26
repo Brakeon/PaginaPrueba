28765eeeb7b56b41c0a4433c858ba2a5f9f09a91
Asier Abad <Asier.abad@gmail.com>
2018-09-26 10:03:13 +0200

    añadidas imagenes

--- Heroe.php
+++ Heroe.php
@@ -27,7 +27,7 @@ class Heroe extends GrupoHeroes
     public function ComprobarUniverso()
     {
         //echo "hola".$nombreGrupo;
-        switch (this->$nombreGrupo) {
+        switch ($this->$nombreGrupo) {
             case "dc":
                 return "dc.png";
                 break;
