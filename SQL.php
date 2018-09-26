da7e5405fb1d4241235d961127ce73961b471ae6
Asier Abad <Asier.abad@gmail.com>
2018-09-26 12:21:56 +0200

    añadidas imagenes

---
+++ SQL.php
@@ -0,0 +1,14 @@
+<?php
+include 'Heroe.php';
+$mysqli =  mysqli_connect('locahost', 'asier', 'asier', 'Heroes');
+
+    if (!$mysqli) {
+        echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+        echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+        echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
+Class DB{
+    
+}
+?>
