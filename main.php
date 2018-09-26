2f853a6343ab3fd5f1300e4f25c63e40c88d6945
Asier Abad <Asier.abad@gmail.com>
2018-09-26 13:39:54 +0200

    sql

--- main.php
+++ main.php
@@ -22,6 +22,7 @@ $arrayHeroe[sizeof($arrayHeroe) + 1] = new Heroe("Deadpool", 1991, "Curación", 
 echo "<h1>hola\n</h1>";
 echo $batman->ComprobarUniverso();
 $db= new DB();
+echo "<h1>holasss\n</h1>";
 echo /*$db->*/Consulta();
 
 foreach ($arrayHeroe as $heroe) {
